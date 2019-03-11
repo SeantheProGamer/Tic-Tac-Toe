82f596b0a9cb8c1f70866775853e52e3e0323d48
slessard19 <lessardsean13@gmail.com>
2019-03-11 10:24:06 -0400

    It works
    yeah

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) 
+    {
+        JFrame window = new JFrame("Tic-Tac-Toe");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().add(new TicTacToe());
+        window.setBounds(300,200,300,300);
+        window.setVisible(true);
+    }
+}
